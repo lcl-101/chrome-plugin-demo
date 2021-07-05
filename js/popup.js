87ec4c8d52ab0699189eece49bc6f1f620f41d41
lichenglong04 <lichenglong04@daojia-inc.com>
2021-07-05 10:07:35 +0800

    ++

--- js/popup.js
+++ js/popup.js
@@ -4,12 +4,12 @@ $(function(){
   	chrome.tabs.create({url: 'https://lcl101.cn/'});
   });
   $('#open_url_new_tab_pi').click(() => {
-  	chrome.tabs.create({url: 'http://10.147.17.110/pi-dashboard/'});
+  	chrome.tabs.create({url: 'http://10.147.17.201/pi-dashboard/'});
   });
   $('#open_url_new_tab_partainer').click(() => {
-  	chrome.tabs.create({url: 'http://10.147.17.110:9000/#/home'});
+  	chrome.tabs.create({url: 'http://10.147.17.201:9000/#/home'});
   });
   $('#open_url_new_tab_explorer').click(() => {
-  	chrome.tabs.create({url: 'http://10.147.17.110:5210/index.php?explorer'});
+  	chrome.tabs.create({url: 'http://10.147.17.201:5210/index.php?explorer'});
   });
 });
